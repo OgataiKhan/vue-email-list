f0a9d909df16e17e0a2badadc614459e5e704de5
OgataiKhan <badellino.kevin@gmail.com>
2023-12-05 15:58:14 +0100

    feat: print emails after all api calls

--- js/main.js
+++ js/main.js
@@ -10,10 +10,15 @@ createApp({
     }
   },
   created() {
+    // Create empty emailPromises array
+    const emailPromises = [];
+    // Call the API 10 times and push the results into the emailPromises array
     for (let i = 0; i < 10; i++) {
-      axios.get(this.apiUrl).then((response) => {
-        this.emails.push(response.data.response);
-      })
-    }
+      emailPromises.push(axios.get(this.apiUrl));
+    };
+    // Use Promise.all to return the 10 promises as a single promise after all the API calls have been made. Use map() to create a separate array of the generated emails and assign it to the "emails" array. 
+    Promise.all(emailPromises).then((responses) => {
+      this.emails = responses.map(response => response.data.response);
+    });
   }
 }).mount('#app');
